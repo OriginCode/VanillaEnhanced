795cd52d12e3204b1d77f31a4c30ddc412b5dd3f
Kaiyang Wu <self@origincode.me>
2022-06-21 16:12:18 -0700

    20220621, update mods

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cd ./packwiz
+cd ./packwiz || exit
 for f in ./mods/*
 do
     mod=${f#./mods/}
     mod=${mod%.pw.toml}
-    echo "$mod"
+    echo "Processing $mod ..."
     packwiz mr install "$mod"
-    if [[ ! $? ]]
+    if [[ $? -ne 0 ]]
     then
+        echo "Installing $mod from CurseForge ..."
         packwiz cf install "$mod"
     fi
 done
